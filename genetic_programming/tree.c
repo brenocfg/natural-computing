9237730191c69908e0185ef06cf11042e7439bde
brenocfg <brenocfg AT gmail DOT com>
2018-09-27 10:02:22 -0300

    Encapsulate tree implementation better.
    Struct definitions were unnecessarily being exposed in the header file. Using
    pimpl is better to encapsulate the tree implementation, given that it will
    likely need to change in later stages of the algorithm implementation.

--- genetic_programming/tree.c
+++ genetic_programming/tree.c
@@ -9,6 +9,36 @@
 #include <limits.h>
 #include <math.h>
 
+/*Domain of possible operators.*/
+enum OP_TYPES { ADD = 1, SUB, MUL, DIV, LOG };
+
+/*Nodes can either be operators, scalar values or variables.*/
+enum NODE_TYPES { OP_TYPE = 0, VAR_TYPE, VAL_TYPE };
+
+/*Let's save some memory and have mutually exclusive members in an union.
+These are:
+op    -> which operator the node is (addition, substraction, etc)
+var   -> the index of which variable the node refers to (x1, x2, etc)
+value -> the constant value the node refers to (real constants such as 0.2)*/
+union node_data {
+	uint8_t op;
+	uint8_t var;
+	double value;
+};
+
+/*A node in our binary tree contains the following members:
+type       -> stores the node type amongst operator/variable/value
+height     -> height of the node in its tree
+left/right -> pointers to its left/right children
+data       -> the semantic data for that node (its value, its operator, etc)*/
+struct node {
+	uint8_t type;
+	uint8_t height;
+	struct node *left, *right;
+
+	union node_data data;
+};
+
 struct node *init_node(uint8_t op, double value, uint8_t height, uint8_t var) {
 	struct node *new_node;
 
